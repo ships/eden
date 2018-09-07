68206521e8189cf5c52ec56b8c1fa7262c0836ef
J Evelyn Quintana <q@world.oberlin.edu>
2018-09-07 13:27:16 -0700

    install ssh-agent using zprofile instead of zshrc

--- install/profile.sh
+++ install/profile.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 repo_root="$(cd `dirname $0`/.. && pwd )"
 
 ln -sf "$repo_root/profile/zshrc" ~/.zshrc
+ln -sf "$repo_root/profile/zprofile" ~/.zprofile
 ln -sf "$repo_root/profile" ~/.eden
 
 open $repo_root/assets/eden.itermcolors
