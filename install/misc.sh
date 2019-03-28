9452c9cc80972c692d3d501bedf5f6032b3584f6
early evening <q@world.oberlin.edu>
2019-03-27 20:15:48 -0700

    allow dynamic concourse version

--- install/misc.sh
+++ install/misc.sh
@@ -3,10 +3,12 @@
 set -euxo pipefail
 repo_root="$(cd `dirname $0`/.. && pwd )"
 
+preferred_concourse_version=${PREFERRED_CONCOURSE_VERSION:-v4.2.3}
+
 function fly_cli() {
   mkdir -p tmp
   aria2c -o tmp/fly \
-    https://github.com/concourse/concourse/releases/download/v3.9.2/fly_darwin_amd64
+    https://github.com/concourse/concourse/releases/download/${preferred_concourse_version}/fly_darwin_amd64
 
   mv tmp/fly /usr/local/bin/fly
   chmod +x /usr/local/bin/fly
@@ -15,7 +17,7 @@ function fly_cli() {
 function concourse_bin() {
   mkdir -p tmp
   aria2c -o tmp/concourse \
-    https://github.com/concourse/concourse/releases/download/v3.9.2/concourse_darwin_amd64
+    https://github.com/concourse/concourse/releases/download/${preferred_concourse_version}/concourse_darwin_amd64
 
 
   mv tmp/concourse /usr/local/bin/concourse
