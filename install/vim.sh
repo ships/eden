5b522990923d4ee88e7c381627ef55264429b8b3
early evening <q@world.oberlin.edu>
2019-04-24 14:22:36 -0700

    fix rust deoplete

--- install/vim.sh
+++ install/vim.sh
@@ -3,6 +3,10 @@
 set -euxo pipefail
 repo_root="$(cd `dirname $0`/.. && pwd )"
 
+ln -s "${repo_root}/assets/vimrc.local.before" "$HOME/.vimrc.local.before"
+ln -s "${repo_root}/assets/vimrc.local" "$HOME/.vimrc.local"
+ln -s "${repo_root}/assets/vimrc.local.plugins" "$HOME/.vimrc.local.plugins"
+
 curl vimfiles.luan.sh/install | bash
 
 ln -sf /usr/local/bin/{n,}vim
