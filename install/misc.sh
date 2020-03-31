915a524ec4d82205e8b542ec2d6a259570ca4755
early evening <q@world.oberlin.edu>
2020-03-30 18:21:17 -0700

    install algorand components

--- install/misc.sh
+++ install/misc.sh
@@ -3,12 +3,14 @@
 set -euxo pipefail
 repo_root="$(cd `dirname $0`/.. && pwd )"
 
-preferred_concourse_version=${PREFERRED_CONCOURSE_VERSION:-v4.2.3}
+preferred_concourse_version=${PREFERRED_CONCOURSE_VERSION:-6.0.0}
+preferred_algorand_version=${PREFERRED_ALGORAND_VERSION:-2.0.5}
 
 function fly_cli() {
   mkdir -p tmp
-  aria2c -o tmp/fly \
-    https://github.com/concourse/concourse/releases/download/${preferred_concourse_version}/fly_darwin_amd64
+  aria2c -o tmp/fly.tgz \
+    https://github.com/concourse/concourse/releases/download/v${preferred_concourse_version}/fly-${preferred_concourse_version}-darwin-amd64.tgz
+  tar -xzvf tmp/fly.tgz -C tmp
 
   mv tmp/fly /usr/local/bin/fly
   chmod +x /usr/local/bin/fly
@@ -16,12 +18,24 @@ function fly_cli() {
 
 function concourse_bin() {
   mkdir -p tmp
-  aria2c -o tmp/concourse \
-    https://github.com/concourse/concourse/releases/download/${preferred_concourse_version}/concourse_darwin_amd64
+  aria2c -o tmp/concourse.tgz \
+    https://github.com/concourse/concourse/releases/download/v${preferred_concourse_version}/concourse-${preferred_concourse_version}-darwin-amd64.tgz
+  tar -xzvf tmp/concourse.tgz -C tmp
 
 
   mv tmp/concourse /usr/local/bin/concourse
   chmod +x /usr/local/bin/concourse
+}
+
+function algorand_bin() {
+  mkdir -p tmp
+  aria2c -o tmp/algorand.tgz \
+    https://github.com/algorand/go-algorand/releases/download/v${preferred_algorand_version}-stable/node_stable_darwin-amd64_${preferred_algorand_version}.tar.gz
+
+  tar -xzvf tmp/algorand.tgz -C tmp/
+  mkdir -p ~/algorand-node
+  rm -rf ~/algorand-node/bin
+  mv tmp/bin ~/algorand-node
 }
 
 function oh_my_zsh() {
@@ -36,6 +50,7 @@ function update_screencap_location {
 }
 
 oh_my_zsh
+algorand_bin
 fly_cli
 concourse_bin
 update_screencap_location
