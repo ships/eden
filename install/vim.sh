5ecf6527b4294476064aaa9bd1dc145881e1f7b6
eve n.u <eve@eatbigger.fish>
2023-12-25 16:44:23 -0800

    latest vim pathing

--- install/vim.sh
+++ install/vim.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 repo_root="$(cd `dirname $0`/.. && pwd )"
 
 ln -sf "${repo_root}/assets/dotvim" "$HOME/.vim"
+ln -sf "${repo_root}/assets/dotvim" "$HOME/.config/nvim"
 
-ln -sf /usr/local/bin/{n,}vim
+ln -sf /opt/homebrew/bin/nvim  /usr/local/bin/vim
 
