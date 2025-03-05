c98ce9751953ce952b8216950b472869467c03df
eve n.u <ships@users.noreply.github.com>
2025-03-05 11:00:01 -0800

    include osx-only python install in osx.sh

--- install/osx.sh
+++ install/osx.sh
@@ -3,6 +3,8 @@
 set -euxo pipefail
 repo_root="$(cd `dirname $0`/.. && pwd )"
 
+sudo echo "This script requires root privileges."
+
 $repo_root/install/brew.sh || echo "Proceeding despite some brew failures"
 $repo_root/install/misc.sh
 $repo_root/install/profile.sh
@@ -15,3 +17,7 @@ $repo_root/install/tmux.sh
 $repo_root/install/ssh.sh
 $repo_root/install/rust.sh
 
+ln -sf /usr/bin/python3 /usr/local/bin/python
+python -m ensurepip
+python -m pip install --upgrade pip
+python -m pip install neovim
